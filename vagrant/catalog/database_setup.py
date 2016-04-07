6ce3e9df064beb1105cdeaf9265b94910d8b56d8
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-04-07 18:32:43 -0400

    fix: Fix typoes

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -2,11 +2,11 @@
 import sys
 
 # Will help with Mapper code
-from sqlalchemy import
+from sqlalchemy import\
 Column, ForeignKey, Integer, String
 
 # Used in config and class code
-from sqlalchemy.ext.declarative import
+from sqlalchemy.ext.declarative import\
 declarative_base
 
 # for mapper ForeignKey relationships
@@ -33,18 +33,20 @@ class MenuItem(Base):
 
     __tablename__ = 'menu_item'
 
-    name = Column(String(80, nullable = False)
+    name = Column(String(80), nullable = False)
+
     id = Column(Integer, primary_key = True)
+
     course = Column(String(250))
     description = Column(String(250))
-    price = Column(string(8))
+    price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
 ######## insert at end of file ############
 
 engine = create_engine(
-'sqlite://restaurantmenu.db')
+'sqlite:///restaurantmenu.db')
 
 # Adds classes as new tables in our database
 Base.metadata.create_all(engine)
