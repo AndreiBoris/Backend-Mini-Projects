728465489d7c8b5b66876b261869059fbfc38dcf
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-04-08 20:53:35 -0400

    feat: List all restaurants in a list /restaurants

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,3 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+# makes the connection between our class definitions and corresponding tables in
+# the database
+Base.metadata.bind = engine
+# A link of communication between our code executions and the engine created above
+DBSession = sessionmaker(bind = engine)
+# In order to create, read, update, or delete information on our database,
+# SQLAlchemy uses sessions. These are basically just transactions. WE can write
+# a bunch of commands and only send them when necessary.
+#
+# You can call methods from within session (below) to make changes to the
+# database. This provides a staging zone for all objects loaded into the
+# database session object. Until we call session.commit(), no changes will be
+# persisted into the database.
+session = DBSession()
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 # Common gateway interface library to decipher messages sent from server
 import cgi
@@ -10,6 +29,37 @@ template_HTML_OPEN = '<body><html>'
 
 template_HTML_CLOSE = '</body></html>'
 
+template_RESTAURANT_OPEN = '<div>'
+
+template_LINE_BREAK = '<br />'
+
+template_RESTAURANT = '<div>%s</div>'
+
+template_RESTAURANT_CLOSE = '</div>'
+
+def ListAllRestaurants(env):
+    # Indicate successful GET request
+    env.send_response(200)
+    # Indicate that the reply is in the form of HTML to the client
+    env.send_header('Content-type', 'text/html')
+    # We are done sending headers
+    env.end_headers()
+
+    restaurantList = session.query(Restaurant).all()
+    output = ''
+    output += template_HTML_OPEN
+
+    for restaurant in restaurantList:
+        output += template_RESTAURANT % restaurant.name
+        output += template_LINE_BREAK
+
+    output += template_FORM
+    output += template_HTML_CLOSE
+    # Send a message back to the client
+    env.wfile.write(output)
+    print output
+    return
+
 # Handler
 class webserverHandler(BaseHTTPRequestHandler):
     # Handle all GET requests that our server receives
@@ -17,25 +67,10 @@ class webserverHandler(BaseHTTPRequestHandler):
         try:
             # BaseHTTPRequestHandler provides variable path that contains urls
             # sent by the client to the server as a string. Find the url that
-            # ends with '/hello'
-            if self.path.endswith('/hello'):
-                # Indicate successful GET request
-                self.send_response(200)
-                # Indicate that the reply is in the form of HTML to the client
-                self.send_header('Content-type', 'text/html')
-                # We are done sending headers
-                self.end_headers()
+            # ends with '/restaurant'
+            if self.path.endswith('/restaurants'):
+                ListAllRestaurants(self)
 
-
-                output = ''
-                output += template_HTML_OPEN
-                output += 'Hello! <a href="/hola">Over to Hola</a>'
-                output += template_FORM
-                output += template_HTML_CLOSE
-                # Send a message back to the client
-                self.wfile.write(output)
-                print output
-                return
             elif self.path.endswith('/hola'):
                 # Indicate successful GET request
                 self.send_response(200)
