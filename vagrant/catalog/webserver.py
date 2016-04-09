f430f0f03793a4b67508415c0a6fac7df024e8ca
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-04-08 21:55:26 -0400

    feat: Can add new restaurants to the database

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -43,7 +43,19 @@ template_RESTAURANT = '''\
 </div>
 '''
 
+template_NEW_RESTAURANT_LINK = '<a href="/restaurants/new">Add Restaurant</a>'
+
+template_NEW_RESTAURANT_FORM = '''\
+<form method="POST" enctype="multipart/form-data" action="/restaurants/adding">
+    <input name="new-restaurant"  type="text">
+    <br />
+    <button type="submit">Add Restaurant</button>
+</form>
+'''
+
 template_RESTAURANT_CLOSE = '</div>'
+
+template_LIST_RESTAURANTS_LINK = '<a href="/restaurants">Back to Restaurants</a>'
 
 def ListAllRestaurants(env):
     # Indicate successful GET request
@@ -56,6 +68,9 @@ def ListAllRestaurants(env):
     restaurantList = session.query(Restaurant).all()
     output = ''
     output += template_HTML_OPEN
+    output += template_NEW_RESTAURANT_LINK
+    output += template_LINE_BREAK
+    output += template_LINE_BREAK
 
     for restaurant in restaurantList:
         output += template_RESTAURANT % {
@@ -66,8 +81,49 @@ def ListAllRestaurants(env):
     output += template_HTML_CLOSE
     # Send a message back to the client
     env.wfile.write(output)
+    # print output
+    return
+
+def CreateNewRestaurant(env):
+    # Indicate successful GET request
+    env.send_response(200)
+    # Indicate that the reply is in the form of HTML to the client
+    env.send_header('Content-type', 'text/html')
+    # We are done sending headers
+    env.end_headers()
+
+    output = ''
+    output += template_HTML_OPEN
+    output += template_LIST_RESTAURANTS_LINK
+    output += template_LINE_BREAK
+    output += template_LINE_BREAK
+    output += template_NEW_RESTAURANT_FORM
+    output += template_HTML_CLOSE
+    # Send a message back to the client
+    env.wfile.write(output)
     print output
     return
+
+def InsertRestaurant(env):
+    # cgi.parse_header parses an HTML header such as 'content-type' into
+    # a main value, and a dictionary of parameters
+    ctype, pdict = cgi.parse_header(env.headers.getheader('content-type'))
+    # Is this form data being received?
+    if ctype == 'multipart/form-data':
+        # cgi.parse_multipart collects all the fields in a form
+        fields = cgi.parse_multipart(env.rfile, pdict)
+        # Get the value from the specific field called 'new-restaurant'
+        restaurantFields = fields.get('new-restaurant')
+        restaurantName = restaurantFields[0]
+        if restaurantName: # if a restaurant name was entered
+            print restaurantName
+            newRestaurant = Restaurant(name = restaurantName)
+            session.add(newRestaurant)
+            session.commit()
+        else:
+            print 'No restaurant name was entered.'
+    else:
+        print 'ctype was not \'multipart/form-data\''
 
 # Handler
 class webserverHandler(BaseHTTPRequestHandler):
@@ -76,9 +132,12 @@ class webserverHandler(BaseHTTPRequestHandler):
         try:
             # BaseHTTPRequestHandler provides variable path that contains urls
             # sent by the client to the server as a string. Find the url that
-            # ends with '/restaurant'
+            # ends with '/restaurants'
             if self.path.endswith('/restaurants'):
                 ListAllRestaurants(self)
+
+            elif self.path.endswith('/restaurants/new'):
+                CreateNewRestaurant(self)
 
             elif self.path.endswith('/hola'):
                 # Indicate successful GET request
@@ -104,30 +163,38 @@ class webserverHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            # incicate successful POST
-            self.send_response(301)
-            self.end_headers()
+            if self.path.endswith('/restaurants/adding'):
+                # redirect to the restaurant list
+                self.send_response(301)
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+                InsertRestaurant(self)
 
-            # cgi.parse_header parses an HTML header such as 'content-type' into
-            # a main value, and a dictionary of parameters
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            # Is this form data being received?
-            if ctype == 'multipart/form-data':
-                # cgi.parse_multipart collects all the fields in a form
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                # Get the value from the specific field called 'message'
-                messagecontent = fields.get('message')
+            else:
+                # incicate successful POST
+                self.send_response(301)
+                self.end_headers()
 
-            output = ''
-            output += '<html><body>'
-            output += '<h2> Okay, how about this: </h2>'
-            output += '<h1> %s </h1>' % messagecontent[0]
+                # cgi.parse_header parses an HTML header such as 'content-type' into
+                # a main value, and a dictionary of parameters
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                # Is this form data being received?
+                if ctype == 'multipart/form-data':
+                    # cgi.parse_multipart collects all the fields in a form
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    # Get the value from the specific field called 'message'
+                    messagecontent = fields.get('message')
 
-            output += template_FORM
-            output += template_HTML_CLOSE
-            self.wfile.write(output)
-            print output
-            return
+                output = ''
+                output += '<html><body>'
+                output += '<h2> Okay, how about this: </h2>'
+                output += '<h1> %s </h1>' % messagecontent[0]
+
+                output += template_FORM
+                output += template_HTML_CLOSE
+                self.wfile.write(output)
+                print output
+                return
         except:
             pass
 
