81fa6354b7ccc2b2be6947aaac74afaafdef3aa1
Andrei Borissenko <Andrei.Borissenko@gmail.com>
2016-04-08 20:10:40 -0400

    feat: POST functionality on the simple server

--- vagrant/catalog/webserver.py
+++ vagrant/catalog/webserver.py
@@ -1,4 +1,14 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+# Common gateway interface library to decipher messages sent from server
+import cgi
+
+template_FORM = '<form method="POST" enctype="multipart/form-data" action="\
+    /hello"><h2>What would you like me to say?</h2><input name="message"\
+    type="text"><input type="submit" value="Submit"></form>'
+
+template_HTML_OPEN = '<body><html>'
+
+template_HTML_CLOSE = '</body></html>'
 
 # Handler
 class webserverHandler(BaseHTTPRequestHandler):
@@ -18,7 +28,10 @@ class webserverHandler(BaseHTTPRequestHandler):
 
 
                 output = ''
-                output += '<html><body>Hello! <a href="/hola">Over to Hola</a></body></html>'
+                output += template_HTML_OPEN
+                output += 'Hello! <a href="/hola">Over to Hola</a>'
+                output += template_FORM
+                output += template_HTML_CLOSE
                 # Send a message back to the client
                 self.wfile.write(output)
                 print output
@@ -33,15 +46,48 @@ class webserverHandler(BaseHTTPRequestHandler):
 
 
                 output = ''
-                output += '<html><body>&#161Hola! <a href="/hello">Back to Hello</a></body></html>'
+                output += template_HTML_OPEN
+                output += '&#161Hola! <a href="/hello">Back to Hello</a>'
+                output += template_FORM
+                output += template_HTML_CLOSE
                 # Send a message back to the client
                 self.wfile.write(output)
                 print output
                 return
-
         except IOError:
             # If path points to something we can't find, we talk about it.
             self.send_error(404, 'File Not Found %s', self.path)
+
+    def do_POST(self):
+        try:
+            # incicate successful POST
+            self.send_response(301)
+            self.end_headers()
+
+            # cgi.parse_header parses an HTML header such as 'content-type' into
+            # a main value, and a dictionary of parameters
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            # Is this form data being received?
+            if ctype == 'multipart/form-data':
+                # cgi.parse_multipart collects all the fields in a form
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                # Get the value from the specific field called 'message'
+                messagecontent = fields.get('message')
+
+            output = ''
+            output += '<html><body>'
+            output += '<h2> Okay, how about this: </h2>'
+            output += '<h1> %s </h1>' % messagecontent[0]
+
+            output += template_FORM
+            output += template_HTML_CLOSE
+            self.wfile.write(output)
+            print output
+            return
+        except:
+            pass
+
+
 
 def main():
     try:
